f74ed7ae535c2b01ae47ab5adf94d5d6d59c984d
donkey103 <donkey103@naver.com>
2024-07-07 15:28:31 +0900

    제출형식에 맞게 출력 제거

--- lv7/2738.py
+++ lv7/2738.py
@@ -7,13 +7,10 @@ for i in range(n * 2):
     a.append(row)
   else:
     b.append(row)
-print(a)
-print(b)
 c = [[0 for _ in range(m)] for _ in range(n)]
 for row in range(n):
   for col in range(m):
     c[row][col] = a[row][col] + b[row][col]
-print(c)
 
 for row in range(n):
   for col in range(m):
