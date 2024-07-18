b028592d562a18742dd1dabb1389748acb4aee2b
JoHyeongjoo <johj@mirae-it.co.kr>
2024-07-19 07:50:39 +0900

    개선된 분해합 알고리즘

---
+++ lv12/2231-2.py
@@ -0,0 +1,18 @@
+def getDigitSum(num):
+  sum = 0
+  while num > 0:
+    sum += num % 10
+    num //= 10
+  return sum
+
+num = int(input())
+constructors = []
+for i in range(num - 1, max(num - 55, 0), - 1):
+
+  if (i + getDigitSum(i))  == num:
+    constructors.append(i)
+print(constructors)
+if len(constructors) == 0:
+  print(0)
+else:
+  print(constructors[-1])
