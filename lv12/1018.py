92d5745443359c87e45bf4c811fd01817c943631
JoHyeongjoo <johj@mirae-it.co.kr>
2024-08-21 12:30:29 +0900

    체스판 하다 맘 ㅅㅂ

---
+++ lv12/1018.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+board = []
+chess = []
+
+for _ in range(n):
+  board.append(input())
+
+for a in range(0, 8):
+  for b in range(0, 8):
+    if board[0][0] == 'W':
+      pass
+    else:
+      pass
