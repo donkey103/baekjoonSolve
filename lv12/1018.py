154bdd41184e52b7e53f5812048f8c9413d3d6b8
donkey103 <donkey103@naver.com>
2026-01-15 17:01:25 +0900

    8x8 체스판에 대한 기본로직 구현

--- lv12/1018.py
+++ lv12/1018.py
@@ -1,13 +1,31 @@
-n, m = map(int, input().split())
+#행, 열 입력
+N, M = map(int, input().split())
 board = []
 chess = []
 
-for _ in range(n):
-  board.append(input())
+for _ in range(N):
+  board.append(list(input()))
 
-for a in range(0, 8):
-  for b in range(0, 8):
-    if board[0][0] == 'W':
-      pass
-    else:
-      pass
+def checkChessBoard(chessBoard):
+  bcounter = 0
+  wcounter = 0
+  # 첫 칸을 B로 가정
+  for N in range(8):
+    for M in range(8):
+      if (N + M) % 2 == 0 and chessBoard[N][M] == 'W':
+        bcounter = bcounter + 1
+      if (N + M) % 2 == 1 and chessBoard[N][M] == 'B':
+        bcounter = bcounter + 1
+    
+  # 첫 칸을 W로 가정
+  for N in range(8):
+    for M in range(8):
+      if (N + M) % 2 == 0 and chessBoard[N][M] == 'B':
+        wcounter = wcounter + 1
+      if (N + M) % 2 == 1 and chessBoard[N][M] == 'W':
+        wcounter = wcounter + 1
+  print(f'bcounter: {bcounter}, wcounter: {wcounter}')
+  return min(bcounter, wcounter)
+
+print(checkChessBoard(board))
+
